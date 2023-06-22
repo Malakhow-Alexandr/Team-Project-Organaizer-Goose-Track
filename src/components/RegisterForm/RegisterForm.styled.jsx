7cebef4095f9fafbfc862886535e33591962ab2f
KobzarAnatolii <TohaPirat@gmail.com>
2023-06-22 12:42:40 +0300

    add styles to register form

--- src/components/RegisterForm/RegisterForm.styled.jsx
+++ src/components/RegisterForm/RegisterForm.styled.jsx
@@ -12,6 +12,15 @@ export const Form = styled(FormikForm)`
   border-radius: ${props => props.theme.radii.small};
   color: ${props => props.theme.colors.black};
   background-color: ${props => props.theme.colors.background};
+
+  @media screen and (min-width: 768px) {
+    max-width: 400px;
+    padding: 40px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    width: 500px;
+  }
 `;
 
 export const FormTitle = styled.h2`
@@ -25,6 +34,12 @@ export const FormTitle = styled.h2`
   color: ${props => props.theme.colors.primary};
   text-shadow: 0px 47px 355px rgba(0, 0, 0, 0.07),
     0px 9.4px 57.6875px rgba(0, 0, 0, 0.035);
+
+  @media screen and (min-width: 768px) {
+    font-size: 24px;
+    text-shadow: 0px 9.399999618530273px 57.6875px 0px rgba(0, 0, 0, 0.04),
+      0px 47px 355px 0px rgba(0, 0, 0, 0.07);
+  }
 `;
 
 export const FormLabel = styled.label`
@@ -36,8 +51,22 @@ export const FormLabel = styled.label`
   font-family: ${props => props.theme.fonts.heading};
   font-style: ${props => props.theme.fonts.heading};
   font-weight: ${props => props.theme.fontWeights.medium};
-  font-size: ${props => props.theme.fontSizes.m};
+  font-size: ${props => props.theme.fontSizes.xs};
   color: ${props => props.theme.colors.black};
+
+  :last-of-type {
+    margin-bottom: 28px;
+  }
+
+  @media screen and (min-width: 768px) {
+    font-size: ${props => props.theme.fontSizes.s};
+    text-shadow: 0px 9.399999618530273px 57.6875px 0px rgba(0, 0, 0, 0.04),
+      0px 47px 355px 0px rgba(0, 0, 0, 0.07);
+
+    :last-of-type {
+      margin-bottom: 48px;
+    }
+  }
 `;
 
 export const Field = styled(FormField)`
@@ -45,13 +74,18 @@ export const Field = styled(FormField)`
   font-family: ${props => props.theme.fonts.heading};
   font-style: ${props => props.theme.fonts.heading};
   font-weight: ${props => props.theme.fontWeights.medium};
-  font-size: ${props => props.theme.fontSizes.m};
+  font-size: ${props => props.theme.fontSizes.s};
   color: ${props => props.theme.colors.black};
   border: ${props => props.theme.borders.normal} rgba(220, 227, 229, 0.6);
   border-radius: 8px;
 
   ::placeholder {
     font-weight: 400;
+  }
+
+  @media screen and (min-width: 768px) {
+    padding: 18px;
+    font-size: ${props => props.theme.fontSizes.m};
   }
 `;
 
@@ -63,12 +97,12 @@ export const Button = styled.button`
   align-items: center;
   justify-content: center;
   width: 100%;
-  height: 56px;
+  height: 46px;
 
   font-family: ${props => props.theme.fonts.heading};
   font-style: ${props => props.theme.fonts.heading};
   font-weight: ${props => props.theme.fontWeights.medium};
-  font-size: 18px;
+  font-size: ${props => props.theme.fontSizes.s};
   line-height: 1.33;
   color: ${props => props.theme.colors.white};
 
@@ -82,5 +116,10 @@ export const Button = styled.button`
   &:hover,
   &:focus {
     background: ${props => props.theme.colors.secondary};
+  }
+
+  @media screen and (min-width: 768px) {
+    height: 56px;
+    font-size: 18px;
   }
 `;
