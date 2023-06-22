f43ce796811546aa52b7bd95ac1ecf8e948d1072
KobzarAnatolii <TohaPirat@gmail.com>
2023-06-22 10:34:23 +0300

    test commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,16 @@
-import { LoginForm } from './LoginForm/LoginForm';
-import { RegisterForm } from './RegisterForm/RegisterForm';
-
 export const App = () => {
   return (
     <div
-    // style={{
-    //   height: '100vh',
-    //   display: 'flex',
-    //   justifyContent: 'center',
-    //   alignItems: 'center',
-    //   fontSize: 40,
-    //   color: '#010101',
-    // }}
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
     >
-      {/* <p>Hello</p> */}
-      <RegisterForm />
-      <LoginForm />
+      <p>Hello</p>
     </div>
   );
 };
