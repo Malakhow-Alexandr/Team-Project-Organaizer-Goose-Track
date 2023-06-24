94ead6f72e522b9e9ea3a4fe508c9f660220eab6
Malakhow-Alexandr <chuk3pc@gmail.com>
2023-06-24 15:00:41 +0300

    update modern normalize

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { theme } from 'theme';
 import { App } from 'components/App';
+import 'modern-normalize';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
