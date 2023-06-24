6d3ce9dc85c6a52d82ee5824e6044556bc08c141
BohdanHromchak <gromchakbogdan@gmail.com>
2023-06-24 17:48:45 +0300

    add files

---
+++ src/components/UserForm/UserForm.styled.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+
+export const Form = styled.form`
+  margin: 20px;
+  width: 300px;
+  padding: 8px;
+  border: 1px solid #2a2a2a;
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+`;
+
+export const FormField = styled.label`
+  display: flex;
+  flex-direction: column;
+`;
