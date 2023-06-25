7b5b14116e3b28c2c86b9a461607318bdedb88ee
BohdanHromchak <gromchakbogdan@gmail.com>
2023-06-25 15:49:38 +0300

    upd

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -3,11 +3,23 @@ import * as Yup from 'yup';
 import { useFormik } from 'formik';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import { AiFillPlusCircle, AiOutlineUser } from 'react-icons/ai';
-import { Container, Form, FormField, Avatar  } from './UserForm.styled';
 import moment from 'moment/moment';
-import defaultAvatar from '../../images/userForm/default-avatar.png'
+import {
+  Container,
+  Form,
+  InputName,
+  Avatar,
+  AddIcon,
+  AvatarDefault,
+  SubmitBtn,
+  UserName,
+  Text,
+  StyledDataPicker,
+  Input,
+  ErrorMessage,
+  Wrapper,
+  WrapperInput,
+} from './UserForm.styled';
 
 const phoneRegexp = /^(\d{2})\s\((\d{3})\)\s(\d{3})\s(\d{2})\s(\d{2})$/;
 const skypeNumberRegexp = /^\+[1-9]\d{0,2}[.-]?\d{1,14}$/;
@@ -41,15 +53,15 @@ export const validationSchema = Yup.object().shape({
 const user = {
   name: 'initialName',
   email: 'initialEmail@mail.com',
-  // avatar:
-  //   'https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o=',
+  avatar:
+    'https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o=',
   phone: '38 (123) 456 78 90',
   birthday: '23/06/2023',
   skype: '+123456789',
 };
 
-const avatarDefault =
-  'https://thumbs.dreamstime.com/b/businessman-icon-vector-male-avatar-profile-image-profile-businessman-icon-vector-male-avatar-profile-image-182095609.jpg';
+// const avatarDefault =
+//   'https://thumbs.dreamstime.com/b/businessman-icon-vector-male-avatar-profile-image-profile-businessman-icon-vector-male-avatar-profile-image-182095609.jpg';
 
 const UserForm = () => {
   const [userAvatar, setUserAvatar] = useState(null || user.avatar);
@@ -145,98 +157,107 @@ const UserForm = () => {
           {userAvatar ? (
             <img src={userAvatar} alt="Avatar" />
           ) : (
-            <img src={defaultAvatar} alt="Avatar" />
-            // <AiOutlineUser/>
+            <AvatarDefault />
           )}
         </Avatar>
-        <FormField>
+
+        <InputName>
           <input
             name="avatar"
             type="file"
             onChange={handleAvatarUpload}
             style={{ display: 'none' }}
           />
-          <AiFillPlusCircle />
-        </FormField>
-        <div>{user.name}</div>
-        <div>User</div>
+          <AddIcon />
+        </InputName>
+        <UserName>{user.name}</UserName>
+        <Text>User</Text>
         {/* username */}
-        <FormField>
-          User Name
-          <input
-            name="name"
-            placeholder="User Name"
-            value={values.name || ''}
-            onChange={handleInputChange}
-            onBlur={handleBlur}
-            className={errors.name && touched.name ? 'InvalidInput' : ''}
-          />
-          {errors.name && touched.name && <div>{errors.name}</div>}
-        </FormField>
-        {/* birthday */}
-        <FormField>
-          Birthday
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DatePicker
-              closeOnSelect={true}
-              slotProps={{
-                textField: {
-                  placeholder: birthdayDate || `${currentDate}`,
-                },
-              }}
-              onChange={handleDatePicker}
-              name="birthday"
-              views={['year', 'month', 'day']}
-              format="DD/MM/YYYY"
+        <Wrapper>
+          <WrapperInput>
+            <InputName>User Name</InputName>
+            <Input
+              name="name"
+              placeholder="User Name"
+              value={values.name || ''}
+              onChange={handleInputChange}
+              onBlur={handleBlur}
+              className={errors.name && touched.name ? 'InvalidInput' : ''}
             />
-          </LocalizationProvider>
-        </FormField>
-        {/* email */}
-        <FormField>
-          Email
-          <input
-            name="email"
-            value={values.email || ''}
-            placeholder="Email"
-            onChange={handleInputChange}
-            onBlur={handleBlur}
-            className={errors.email && touched.email ? 'InvalidInput' : ''}
-          />
-          {errors.email && touched.email && <div>{errors.email}</div>}
-        </FormField>
-        {/* phone */}
-        <FormField>
-          Phone
-          <input
-            name="phone"
-            placeholder="ex. 01 (234) 567 89 01"
-            value={values.phone || ''}
-            onChange={autoFormatPhoneNumber}
-            onBlur={handleBlur}
-            className={errors.phone && touched.phone ? 'InvalidInput' : ''}
-          />
-          {errors.phone && touched.phone && <div>{errors.phone}</div>}
-        </FormField>
-        {/* skype */}
-        <FormField>
-          Skype
-          <input
-            name="skype"
-            value={values.skype || ''}
-            placeholder="ex. +1234567890"
-            onChange={handleInputChange}
-            onBlur={handleBlur}
-            className={errors.skype && touched.skype ? 'InvalidInput' : ''}
-          />
-          {errors.skype && touched.skype && <div>{errors.skype}</div>}
-        </FormField>
-        <button disabled={!isFormChanged} type="submit">
+            {errors.name && touched.name && (
+              <ErrorMessage>{errors.name}</ErrorMessage>
+            )}
+          </WrapperInput>
+          {/* birthday */}
+          <WrapperInput>
+            <InputName>Birthday</InputName>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <StyledDataPicker
+                closeOnSelect={true}
+                slotProps={{
+                  textField: {
+                    placeholder: birthdayDate || `${currentDate}`,
+                  },
+                }}
+                onChange={handleDatePicker}
+                name="birthday"
+                views={['year', 'month', 'day']}
+                format="DD/MM/YYYY"
+              />
+            </LocalizationProvider>
+          </WrapperInput>
+          {/* email */}
+          <WrapperInput>
+            <InputName>Email </InputName>
+            <Input
+              name="email"
+              value={values.email || ''}
+              placeholder="Email"
+              onChange={handleInputChange}
+              onBlur={handleBlur}
+              className={errors.email && touched.email ? 'InvalidInput' : ''}
+            />
+            {errors.email && touched.email && (
+              <ErrorMessage>{errors.email}</ErrorMessage>
+            )}
+          </WrapperInput>
+          {/* phone */}
+          <WrapperInput>
+            <InputName>Phone</InputName>
+            <Input
+              name="phone"
+              placeholder="ex. 01 (234) 567 89 01"
+              value={values.phone || ''}
+              onChange={autoFormatPhoneNumber}
+              onBlur={handleBlur}
+              className={errors.phone && touched.phone ? 'InvalidInput' : ''}
+            />
+            {errors.phone && touched.phone && (
+              <ErrorMessage>{errors.phone}</ErrorMessage>
+            )}
+          </WrapperInput>
+          {/* skype */}
+          <WrapperInput>
+            <InputName>Skype</InputName>
+            <Input
+              name="skype"
+              value={values.skype || ''}
+              placeholder="ex. +1234567890"
+              onChange={handleInputChange}
+              onBlur={handleBlur}
+              className={errors.skype && touched.skype ? 'InvalidInput' : ''}
+            />
+            {errors.skype && touched.skype && (
+              <ErrorMessage>{errors.skype}</ErrorMessage>
+            )}
+          </WrapperInput>
+        </Wrapper>
+        <SubmitBtn disabled={!isFormChanged} type="submit">
           Save changes
-        </button>
+        </SubmitBtn>
       </Form>
     </Container>
   );
 };
 
 export default UserForm;
-
--- src/components/UserForm/UserForm.styled.jsx
+++ src/components/UserForm/UserForm.styled.jsx
@@ -1,4 +1,6 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+import { AiFillPlusCircle, AiOutlineUser } from 'react-icons/ai';
+import { DatePicker } from '@mui/x-date-pickers';
 
 export const Container = styled.div`
   max-width: 375px;
@@ -45,7 +47,7 @@ export const Form = styled.form`
 `;
 
 export const Avatar = styled.div`
-    width: 72px;
+  width: 72px;
   height: 72px;
   position: absolute;
   left: calc(50% - 36px);
@@ -69,9 +71,220 @@ export const Avatar = styled.div`
     object-fit: cover;
     border-radius: ${props => props.theme.radii.round};
   }
-`
+`;
 
-export const FormField = styled.label`
+export const InputName = styled.label`
+  font-family: ${props => props.theme.fonts.text};
+  font-size: ${props => props.theme.fontSizes.xs};
+  font-weight: 400;
+  margin-bottom: 8px;
+  line-height: ${props => props.theme.lineHeights.heading};
+  color: ${props => props.theme.colors.user_label_color_switch};
+`;
+export const Input = styled.input`
+  min-width: 250px;
+  height: 42px;
+  padding-left: 14px;
+  padding-right: 14px;
+  outline: none;
+
+  border: ${props => props.theme.colors.user_input_border_color_switch};
+  border-radius: ${props => props.theme.radii.small};
+
+  ${props =>
+    props.value &&
+    `
+    font-family: 'Inter';
+    font-size: 14px;
+    font-weight:600;
+    color: black;
+  
+  `}
+  color: ${props => props.theme.colors.primary_text_switch};
+  background-color: transparent;
+  &::placeholder {
+    font-family: ${props => props.theme.fonts.text};
+    font-size: ${props => props.theme.fontSizes.s};
+    color: ${props => props.theme.colors.user_input_border_color_switch};
+  }
+  &:hover,
+  &:focus {
+    border: 1px solid #111111;
+  }
+  @media (min-width: 768px) {
+    height: 46px;
+  }
+`;
+export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
+  gap: 18px;
+  @media (min-width: 1440px) {
+    width: 758px;
+    flex-direction: initial;
+    flex-wrap: wrap;
+    column-gap: 50px;
+    row-gap: 24px;
+  }
+`;
+
+export const WrapperInput = styled.div`
+  display: flex;
+  flex-direction: column;
+  @media (min-width: 768px) {
+    width: 354px;
+  }
+  @media (min-width: 1440px) {
+    width: 354px;
+  }
+`;
+export const ErrorMessage = styled.div`
+  color: ${props => props.theme.colors.redError};
+  font-size: 12px;
+  margin-top: 3px;
+  margin-left: 8px;
+  @media (min-width: 768px) {
+    margin-top: 4px;
+    font-size: 14px;
+  }
+`;
+export const AddIcon = styled(AiFillPlusCircle)`
+  position: absolute;
+  left: 53%;
+  top: 28px;
+  width: 14px;
+  height: 14px;
+  font-size: 12px;
+  cursor: pointer;
+
+  @media (min-width: 768px) {
+    width: 24px;
+    height: 24px;
+    top: 147px;
+    left: 52%;
+  }
+  @media (min-width: 1440px) {
+    top: 170px;
+    left: 51%;
+  }
+`;
+
+export const AvatarDefault = styled(AiOutlineUser)`
+  position: absolute;
+  left: calc(50% - 25px);
+  top: 5px;
+  width: 50px;
+  height: 50px;
+  @media (min-width: 768px) {
+    left: calc(50% - 47px);
+    top: 45px;
+    width: 95px;
+    height: 95px;
+  }
+  @media (min-width: 1440px) {
+    left: calc(50% - 47px);
+    top: 65px;
+    width: 95px;
+    height: 95px;
+  }
+`;
+
+export const SubmitBtn = styled.button`
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 40px;
+  padding-top: 14px;
+  padding-bottom: 14px;
+  padding-left: 50px;
+  padding-right: 50px;
+  width: 195px;
+  height: 46px;
+  font-family: ${props => props.theme.fonts.text};
+  font-weight: ${props => props.theme.fontWeights.heading};
+  line-height: ${props => props.theme.lineHeights.body};
+  font-size: ${props => props.theme.fontSizes.s};
+  text-align: center;
+  color: ${props => props.theme.colors.white};
+  background-color: ${props =>
+    props.disabled
+      ? props.theme.colors.background_switch
+      : props.theme.colors.primary};
+  border-radius: ${props => props.theme.radii.big};
+
+  border: ${props => props.theme.borders.none};
+  cursor: ${props => (props.disabled ? 'default' : 'pointer')};
+  &:hover(:not disabled),
+  &:focus(:not disabled) {
+    background-color: ${props => props.theme.colors.secondary};
+  }
+
+  @media (min-width: 768px) {
+    width: 262px;
+    height: 48px;
+  }
+  @media (min-width: 1440px) {
+    margin-top: 88px;
+  }
+`;
+
+export const UserName = styled.p`
+  font-family: ${props => props.theme.fonts.heading};
+  font-weight: ${props => props.theme.fontWeights.bold};
+  font-size: ${props => props.theme.fontSizes.s};
+  color: ${props => props.theme.colors.primary_text_switch};
+  text-align: center;
+`;
+
+export const Text = styled.p`
+  margin-top: 4px;
+  font-family: ${props => props.theme.fonts.heading};
+  font-weight: ${props => props.theme.fontWeights.normal};
+  font-size: ${props => props.theme.fontSizes.xs};
+  color: ${props => props.theme.colors.user_label_color_switch};
+  text-align: center;
+`;
+
+export const StyledDataPicker = styled(DatePicker)`
+  input {
+    padding-left: 0px;
+    color: ${props => props.theme.colors.primary_text_switch};
+  }
+  input::placeholder {
+    font-family: ${props => props.theme.fonts.text};
+    font-size: ${props => props.theme.fontSizes.s};
+    color: ${props => props.theme.colors.user_label_color_switch};
+    font-weight: 600;
+    opacity: 1;
+  }
+
+  /* Input value */
+  .MuiInputBase-root {
+    position: relative;
+    font-family: 'Inter';
+    font-size: 14px;
+    font-weight: 600;
+  }
+  .MuiButtonBase-root {
+    color: ${props => props.theme.colors.calendar_border_color_switch};
+  }
+  .MuiOutlinedInput-notchedOutline {
+    border: 1px solid #11111126;
+  }
+
+  .MuiOutlinedInput-root {
+    min-width: 250px;
+    height: 44px;
+    padding-left: 14px;
+    padding-right: 14px;
+    outline: none;
+    border: none;
+    border-radius: ${props => props.theme.radii.small};
+
+    .MuiOutlinedInput-notchedOutline {
+      border: ${props => props.theme.colors.user_input_border_color_switch};
+    }
+    &.Mui-focused .MuiOutlinedInput-notchedOutline {
+      border: 1px solid #111111;
+    }
+  }
 `;
