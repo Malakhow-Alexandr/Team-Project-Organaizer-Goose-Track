b186b8c80baa5088e9c2aa5f7583607f2c812528
Pavlo Berkut <pavlo.berkut@gmail.com>
2023-06-26 04:28:07 +0300

    Add hidden SideBar when click MainLayout

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -2,6 +2,7 @@ import { UserNav } from 'components/UserNav/UserNav';
 import { SideBarContainer, LogoutBtn, LogoutBtnIcon } from './SideBar.styled';
 import { useDispatch } from 'react-redux';
 import { logoutUser } from 'redux/auth/operations';
+import { useEffect, useRef } from 'react';
 
 const SideBar = ({ toggleShowSideBar }) => {
   const dispatch = useDispatch();
@@ -10,8 +11,26 @@ const SideBar = ({ toggleShowSideBar }) => {
     dispatch(logoutUser());
   };
 
+  /*
+Hook для приховування компонента SideBar
+при кліку мишки за межами SideBar
+ */
+  const sideBarRef = useRef(null);
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (sideBarRef.current && !sideBarRef.current.contains(event.target)) {
+        toggleShowSideBar(false);
+      }
+    }
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [toggleShowSideBar]);
+
   return (
-    <SideBarContainer>
+    <SideBarContainer ref={sideBarRef}>
       <UserNav toggleShowSideBar={toggleShowSideBar} />
 
       <LogoutBtn type="button" onClick={handelLogOut}>
