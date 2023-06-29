71e8e7cea60eee356503eeb72fc90d7056e934bc
Julia-Gavrashenko <julia.gavrashenko@gmail.com>
2023-06-29 16:22:39 +0300

    add some logic

--- src/components/AddFeedbackModal/AddFeedbackModal.jsx
+++ src/components/AddFeedbackModal/AddFeedbackModal.jsx
@@ -1,11 +1,38 @@
 import { Modal } from 'components/Modal/Modal';
 import { FeedbackForm } from './FeedbackForm';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { getReviewByOwn } from '../../redux/reviews/operations';
+import { selectReviewByOwn } from '../../redux/reviews/selectors';
 
 export const AddFeedbackModal = ({ onClose }) => {
+  const [userFeedback, setUserFeedback] = useState('');
+  const [userRating, setUserRating] = useState(null);
+
+  const dispatch = useDispatch();
+
+  const userReview = useSelector(selectReviewByOwn);
+  // console.log(userReview)
+  // console.log(userReview.text);
+  // console.log(userReview.rating);
+  // console.log(userReview._id);
+
+  useEffect(() => {
+    dispatch(getReviewByOwn());
+    setUserFeedback(userReview.text);
+    setUserRating(userReview.rating);
+  }, [dispatch, userReview.rating, userReview.text]);
+
   return (
     <Modal onClose={onClose}>
-          <div>
-              <FeedbackForm/>
+      <div>
+        <FeedbackForm
+          userFeedback={userFeedback}
+          userRating={userRating}
+          onClose={onClose}
+        />
       </div>
     </Modal>
   );
--- src/components/AddFeedbackModal/FeedbackForm.jsx
+++ src/components/AddFeedbackModal/FeedbackForm.jsx
@@ -1,19 +1,32 @@
-import { Formik } from 'formik';
+import { Formik, Field } from 'formik';
 import * as Yup from 'yup';
 import { useState } from 'react';
-import {useDispatch } from 'react-redux';
-import {createReviewByOwn} from '../../redux/reviews/operations'
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import {
+  createReviewByOwn,
+  updateReviewByOwn,
+  deleteReviewByOwn,
+} from '../../redux/reviews/operations';
 import { FaStar } from 'react-icons/fa';
 import {
   Form,
   FormField,
   ErrorMessage,
   RatingLabel,
-  RatingInputOverlay,
-  RatingField,
+  // RatingInputOverlay,
+  // RatingField,
   TextareaField,
   RatingBtnOverlay,
   SaveFeedbackBtn,
+  CancelFeedbackBtn,
+  EditFeedbackBtn,
+  EditButtonContainer,
+  ButtonDiv,
+  PencilBtn,
+  TrashBtn,
+  Pencil,
+  Trash,
 } from './FeedbackForm.styled';
 
 const FeedbackFormSchema = Yup.object().shape({
@@ -24,71 +37,122 @@ const FeedbackFormSchema = Yup.object().shape({
     .required('Required'),
 });
 
-export const FeedbackForm = () => {
-  const [feedbackRating, setFeedbackRating] = useState(null);
+export const FeedbackForm = ({
+  userFeedback,
+  userRating,
+  // toggleEditFeedback,
+  // editFeedbackFieldOpen,
+  onClose,
+}) => {
+  console.log(userFeedback);
+  console.log(userRating);
+
+  const [feedbackRating, setFeedbackRating] = useState(0);
+  const [feedbackText, setFeedbackText] = useState('');
   const [ratingHover, setRatingHover] = useState(null);
+  const [editFeedbackFieldOpen, setEditFeedbackFieldOpen] = useState(false);
+  const [isEdit, setIsEdit] = useState(false);
+
+  console.log(feedbackRating);
+  console.log(feedbackText);
 
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (userFeedback || userRating) {
+      setFeedbackRating(userFeedback);
+      setFeedbackText(userRating);
+      setEditFeedbackFieldOpen(!editFeedbackFieldOpen);
+    }
+  }, [userRating, userFeedback]);
 
   const handleFeedbackSubmit = (values, { resetForm }) => {
     console.log(values);
     // console.log(actions);
+    // if (editFeedbackField) {
+    //   dispatch(updateReviewByOwn());
+    //   toggleEditFeedback();
+    //   return;
+    // }
+    if (!userFeedback || !userRating) {
+      dispatch(createReviewByOwn({ text: values.text, rating: values.rating }));
+      resetForm();
+      setFeedbackText(values.text);
+      onClose();
+    }
+  };
 
-    dispatch(createReviewByOwn({text: values.text, rating: values.rating}))
-    resetForm();
+  const handleEditBtnClick = () => {
+    setIsEdit(!isEdit);
+  };
+
+  const handleFeedbackDelete = () => {
+    dispatch(deleteReviewByOwn());
+    onClose();
   };
 
   return (
     <Formik
-      initialValues={{ rating: feedbackRating, text: '' }}
+      initialValues={{ rating: feedbackRating, text: feedbackText }}
       validationSchema={FeedbackFormSchema}
       onSubmit={handleFeedbackSubmit}
     >
       <Form>
         <RatingLabel>Rating</RatingLabel>
-        <FormField>
-          <RatingInputOverlay>
-          {[...Array(5)].map((item, ind) => {
-            const ratingValue = ind + 1;
 
-            return (
-              <div key={ratingValue}>
-                <RatingField
-                 
-                  type="radio"
-                  name="rating"
-                  value={ratingValue}
-                  onClick={() => setFeedbackRating(ratingValue)}
-                  autoComplete="off"
+        <FormField>
+          <div>
+            {[...Array(5)].map((star, ind) => {
+              const ratingValue = ind + 1;
+
+              return (
+                <label key={ratingValue}>
+                  <Field
+                    type="radio"
+                    name="rating"
+                    value={ratingValue}
+                    onClick={() => {
+                      setFeedbackRating(ratingValue);
+                    }}
                     // style={{
                     //   display: 'none',
                     // }}
-                />
-
-                <FaStar
-                 
-                 
-                  size={24}
-                  color={
-                    ratingValue <= (ratingHover || feedbackRating)
-                      ? '#FFAC33'
-                      : '#CEC9C1'
-                  }
-                  onMouseEnter={() => setRatingHover(ratingValue)}
-                  onMouseLeave={() => setRatingHover(null)}
-                  style={{
-                    marginRight: 0,
-                    cursor: 'pointer',
-                  }}
-                />
-              </div>
-            );
-          })}
-            <ErrorMessage name="rating" component="p" />
-            </RatingInputOverlay>
+                  />
+                  <FaStar
+                    size={24}
+                    color={
+                      ratingValue <= (ratingHover || feedbackRating)
+                        ? '#FFAC33'
+                        : '#CEC9C1'
+                    }
+                    onMouseEnter={() => setRatingHover(ratingValue)}
+                    onMouseLeave={() => setRatingHover(null)}
+                  />
+                </label>
+              );
+            })}
+          </div>
         </FormField>
 
-        <RatingLabel>Review</RatingLabel>
+        <EditButtonContainer>
+          {!editFeedbackFieldOpen ? (
+            <RatingLabel>Review</RatingLabel>
+          ) : (
+            <>
+              <RatingLabel>Review</RatingLabel>
+
+              <ButtonDiv>
+                <PencilBtn onClick={handleEditBtnClick}>
+                  <Pencil />
+                </PencilBtn>
+                <TrashBtn onClick={handleFeedbackDelete}>
+                  <Trash />
+                </TrashBtn>
+              </ButtonDiv>
+            </>
+          )}
+        </EditButtonContainer>
+
         <FormField>
           <TextareaField
             component="textarea"
@@ -101,13 +165,15 @@ export const FeedbackForm = () => {
         </FormField>
 
         <RatingBtnOverlay>
-          {/* <button type="submit">Edit</button> */}
-          <SaveFeedbackBtn type="submit">Save</SaveFeedbackBtn>
-          <SaveFeedbackBtn type="submit">Cancel</SaveFeedbackBtn>
+          {editFeedbackFieldOpen ? (
+            <EditFeedbackBtn type="submit">Edit</EditFeedbackBtn>
+          ) : (
+            <SaveFeedbackBtn type="submit">Save</SaveFeedbackBtn>
+          )}
+
+          <CancelFeedbackBtn type="submit">Cancel</CancelFeedbackBtn>
         </RatingBtnOverlay>
       </Form>
     </Formik>
   );
 };
-
-// export default FeedbackForm;
