449c1e50d751d219aa7f495776a3a6b747a57841
Oleksandr FIlippov <filya170895@gmail.com>
2023-06-30 10:50:54 +0200

    Update operations.js

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -38,10 +38,16 @@ instance.interceptors.response.use(
   }
 );
 
-const tostStyle = {
+const tostStyleError = {
   borderRadius: '8px',
   background: '#13151A',
   color: '#3E85F3',
+};
+
+const tostStyleSuccess = {
+  borderRadius: '8px',
+  background: '#3E85F3',
+  color: '#ffffff',
 };
 
 export const registerUser = createAsyncThunk(
@@ -56,12 +62,15 @@ export const registerUser = createAsyncThunk(
       console.log(response.data);
       localStorage.setItem('refreshToken', response.data.refreshToken);
       setToken(response.data.accessToken);
+      toast.success(`Welcome, ${response.data.user.name}!`, {
+        style: tostStyleSuccess,
+      });
       return response.data;
     } catch (error) {
       toast.error(
         `Registration failed!😕 Please check your information and try again!`,
         {
-          style: tostStyle,
+          style: tostStyleError,
         }
       );
       return thunkAPI.rejectWithValue(error.message);
@@ -77,10 +86,13 @@ export const loginUser = createAsyncThunk(
       console.log(response.data);
       localStorage.setItem('refreshToken', response.data.refreshToken);
       setToken(response.data.accessToken);
+      toast.success(`Welcome back, ${response.data.user.name}!`, {
+        style: tostStyleSuccess,
+      });
       return response.data;
     } catch (error) {
       toast.error(`Invalid email or password!😕 Try again!`, {
-        style: tostStyle,
+        style: tostStyleError,
       });
       return thunkAPI.rejectWithValue(error.message);
     }
@@ -128,7 +140,7 @@ export const updateUser = createAsyncThunk(
       const formData = new FormData();
       formData.append('name', name);
       formData.append('email', email);
-      if(avatar !== undefined) {
+      if (avatar !== undefined) {
         formData.append('avatar', avatar);
       }
       formData.append('phone', phone);
