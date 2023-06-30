dbfdccb12a9779735469d2047a31ee333974b5bf
Pavlo Berkut <pavlo.berkut@gmail.com>
2023-07-01 00:29:56 +0300

    Change naimation in loader

--- src/components/Loader/Loader.styled.jsx
+++ src/components/Loader/Loader.styled.jsx
@@ -14,7 +14,6 @@ export const Backdrop = styled.div`
 `;
 
 export const SpinWrap = styled.div`
-  height: 120%;
   position: fixed;
   top: 50%;
   left: 50%;
@@ -26,17 +25,39 @@ export const Spin = styled.span`
   display: block;
   margin: auto;
   position: relative;
-  animation: myAnim 2s linear infinite forwards;
+  animation: myAnim 2s ease infinite;
 
   @keyframes myAnim {
     0% {
-      transform: translateY(800px);
+      transform: scale3d(1, 1, 1);
+    }
+
+    30% {
+      transform: scale3d(1.25, 0.75, 1);
+    }
+
+    40% {
+      transform: scale3d(0.75, 1.25, 1);
+    }
+
+    50% {
+      transform: scale3d(1.15, 0.85, 1);
+    }
+
+    65% {
+      transform: scale3d(0.95, 1.05, 1);
+    }
+
+    75% {
+      transform: scale3d(1.05, 0.95, 1);
     }
 
     100% {
-      transform: translateY(0px);
-      transform: scale(0.5);
-      transform-origin: 50% 0%;
+      transform: scale3d(1, 1, 1);
     }
   }
+`;
+
+export const Image = styled.img`
+  max-width: inherit;
 `;
