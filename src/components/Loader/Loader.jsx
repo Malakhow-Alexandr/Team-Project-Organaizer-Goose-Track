b4c5909e548c62c1b0721260e1fbedc37e54806d
KobzarAnatolii <TohaPirat@gmail.com>
2023-07-02 11:11:01 +0300

    fix loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,17 +1,11 @@
 import React from 'react';
 import { Backdrop, Spin, SpinWrap } from './Loader.styled';
 
-// import ImgRacketGoose from '../../images/loader/racket-goose.webp';
-import { LoaderForBtn } from 'components/LoaderForBtn/LoaderForBtn';
-
 export const Loader = () => {
   return (
     <Backdrop>
       <SpinWrap>
-        <Spin>
-          {/* <Image srcSet={`${ImgRacketGoose}`} alt="Racket Goose" /> */}
-          <LoaderForBtn />
-        </Spin>
+        <Spin></Spin>
       </SpinWrap>
     </Backdrop>
   );
