8124383ab73305e48a08638981d2d8d016f2b6d0
Malakhow-Alexandr <109951660+Malakhow-Alexandr@users.noreply.github.com>
2023-07-04 19:21:28 +0300

    add fix styles

--- src/components/Footer/WavingFlag.jsx
+++ src/components/Footer/WavingFlag.jsx
@@ -7,8 +7,8 @@ export const WavingFlagCustom = () => {
       
       <WavingFlag
         flag="https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Flag_of_Ukraine.svg/1280px-Flag_of_Ukraine.svg.png"
-        height={70}
-        width={120}
+        height={60}
+        width={100}
         animationSpeed={0.9}
         curvature={12}
         isShadow={false}
