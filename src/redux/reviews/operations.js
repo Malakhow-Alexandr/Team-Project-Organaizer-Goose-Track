c05ec3c16aaee1b0ece1351b6d3d1a6b74bc35d0
Oleksandr FIlippov <filya170895@gmail.com>
2023-07-04 13:58:55 +0200

    added notifications to reviews

--- src/redux/reviews/operations.js
+++ src/redux/reviews/operations.js
@@ -1,12 +1,14 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { instance } from '../auth/operations';
+import { toast } from 'react-hot-toast';
+import { tostStyleSuccess } from '../auth/operations';
 
 export const getAllReviews = createAsyncThunk(
   'reviews/getAllReviews',
   async (_, thunkAPI) => {
     try {
       const response = await instance.get('/reviews');
-      console.log(response.data);
+
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
@@ -19,7 +21,7 @@ export const getReviewByOwn = createAsyncThunk(
   async (_, thunkAPI) => {
     try {
       const response = await instance.get('/reviews/own');
-      console.log(response.data);
+
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
@@ -32,7 +34,10 @@ export const createReviewByOwn = createAsyncThunk(
   async ({ text, rating }, thunkAPI) => {
     try {
       const response = await instance.post('/reviews/own', { text, rating });
-      console.log(response.data);
+
+      toast.success('Thank you, we appreciate your feedback!', {
+        style: tostStyleSuccess,
+      });
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
@@ -45,7 +50,10 @@ export const updateReviewByOwn = createAsyncThunk(
   async ({ text, rating }, thunkAPI) => {
     try {
       const response = await instance.patch('/reviews/own', { text, rating });
-      console.log(response.data);
+
+      toast.success('Your review has been successfully updated', {
+        style: tostStyleSuccess,
+      });
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
@@ -58,7 +66,9 @@ export const deleteReviewByOwn = createAsyncThunk(
   async (_, thunkAPI) => {
     try {
       const response = await instance.delete('/reviews/own');
-      console.log(response.data);
+      toast.success('Your review has been successfully deleted', {
+        style: tostStyleSuccess,
+      });
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
